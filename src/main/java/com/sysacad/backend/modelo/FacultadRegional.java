e10aebb6edad7fe15ec13ef5bc8cd363ea084535
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-02 03:56:16 -0300

    Armado Clase Facultad Regional (con su respectivo package)

---
+++ src/main/java/com/sysacad/backend/modelo/FacultadRegional.java
@@ -0,0 +1,21 @@
+package com.sysacad.backend.modelo;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "facultades_regionales")
+public class FacultadRegional {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 100)
+    private String ciudad;
+
+    @Column(nullable = false, length = 100)
+    private String provincia;
+}
