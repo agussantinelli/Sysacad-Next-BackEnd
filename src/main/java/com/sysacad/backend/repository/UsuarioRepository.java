74b2e13cd039540dfb01b25a4ad9f7df91f0f88f
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-03 02:52:58 -0300

    Armo UsuarioRepository + Package Repository

---
+++ src/main/java/com/sysacad/backend/repository/UsuarioRepository.java
@@ -0,0 +1,25 @@
+package com.sysacad.backend.repository;
+
+import com.sysacad.backend.modelo.Usuario;
+import com.sysacad.backend.modelo.enums.RolUsuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+
+    Optional<Usuario> findByLegajo(String legajo);
+    Optional<Usuario> findByMail(String mail);
+    Optional<Usuario> findByDni(String dni);
+
+    List<Usuario> findByRol(RolUsuario rol);
+
+    List<Usuario> findByApellidoContainingIgnoreCase(String apellido);
+
+    boolean existsByMail(String mail);
+    boolean existsByLegajo(String legajo);
+}
