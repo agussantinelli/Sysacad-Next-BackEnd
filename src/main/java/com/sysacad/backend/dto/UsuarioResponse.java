473348b153cb55ef1b9eedd6102cb2a68cf0a28d
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-04 20:43:19 -0300

    NUEVO: UsuarioResponse

---
+++ src/main/java/com/sysacad/backend/dto/UsuarioResponse.java
@@ -0,0 +1,53 @@
+package com.sysacad.backend.dto;
+
+import com.sysacad.backend.modelo.Usuario;
+import com.sysacad.backend.modelo.enums.Genero;
+import com.sysacad.backend.modelo.enums.RolUsuario;
+import com.sysacad.backend.modelo.enums.TipoDocumento;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+public class UsuarioResponse {
+    private UUID id;
+    private String legajo;
+    private TipoDocumento tipoDocumento;
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private String mail;
+    private LocalDate fechaNacimiento;
+    private Genero genero;
+    private String telefono;
+    private String direccion;
+    private String ciudad;
+    private String fotoPerfil;
+    private LocalDate fechaIngreso;
+    private String tituloAcademico;
+    private RolUsuario rol;
+    private String estado;
+
+    private String tipoIdentificador;
+
+    public UsuarioResponse(Usuario usuario) {
+        this.id = usuario.getId();
+        this.legajo = usuario.getLegajo();
+        this.tipoDocumento = usuario.getTipoDocumento();
+        this.dni = usuario.getDni();
+        this.nombre = usuario.getNombre();
+        this.apellido = usuario.getApellido();
+        this.mail = usuario.getMail();
+        this.fechaNacimiento = usuario.getFechaNacimiento();
+        this.genero = usuario.getGenero();
+        this.telefono = usuario.getTelefono();
+        this.direccion = usuario.getDireccion();
+        this.ciudad = usuario.getCiudad();
+        this.fotoPerfil = usuario.getFotoPerfil();
+        this.fechaIngreso = usuario.getFechaIngreso();
+        this.tituloAcademico = usuario.getTituloAcademico();
+        this.rol = usuario.getRol();
+        this.estado = usuario.getEstado();
+    }
+}
