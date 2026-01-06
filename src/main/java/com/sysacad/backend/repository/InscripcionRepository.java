ba98ce46c7750addaca4e5543aa801c946b39de0
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-06 16:24:59 -0300

    Mejora InscripcionRepository

--- src/main/java/com/sysacad/backend/repository/InscripcionRepository.java
+++ src/main/java/com/sysacad/backend/repository/InscripcionRepository.java
@@ -1,7 +1,10 @@
 package com.sysacad.backend.repository;
 
 import com.sysacad.backend.modelo.Inscripcion;
+import com.sysacad.backend.modelo.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +16,10 @@ public interface InscripcionRepository extends JpaRepository<Inscripcion, Inscri
     List<Inscripcion> findByIdIdUsuario(UUID idUsuario);
 
     List<Inscripcion> findByIdIdComision(UUID idComision);
+
+    @Query("SELECT DISTINCT i.usuario FROM Inscripcion i " +
+            "JOIN i.comision c " +
+            "JOIN c.materias m " +
+            "WHERE m.id = :idMateria")
+    List<Usuario> findAlumnosByMateria(@Param("idMateria") UUID idMateria);
 }
