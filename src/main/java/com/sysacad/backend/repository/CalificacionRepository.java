f3d6f1dd85836779e1d7cdb4022c09d560c068bb
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-06 16:12:48 -0300

    Mejoro CalificacionRepository

--- src/main/java/com/sysacad/backend/repository/CalificacionRepository.java
+++ src/main/java/com/sysacad/backend/repository/CalificacionRepository.java
@@ -1,6 +1,7 @@
 package com.sysacad.backend.repository;
 
 import com.sysacad.backend.modelo.Calificacion;
+import com.sysacad.backend.modelo.Calificacion.CalificacionId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,8 +9,9 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface CalificacionRepository extends JpaRepository<Calificacion, Calificacion.CalificacionId> {
+public interface CalificacionRepository extends JpaRepository<Calificacion, CalificacionId> {
 
     List<Calificacion> findByIdIdUsuario(UUID idUsuario);
 
+    List<Calificacion> findByIdIdComision(UUID idComision);
 }
