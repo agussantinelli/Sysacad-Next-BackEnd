5fffa17cadccdf756bc1ce9bedaee7f14ecd946b
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-06 14:58:01 -0300

    NUEVO ENUM: RolCargo

---
+++ src/main/java/com/sysacad/backend/modelo/enums/RolCargo.java
@@ -0,0 +1,6 @@
+package com.sysacad.backend.modelo.enums;
+
+public enum RolCargo {
+    JEFE_CATEDRA, // Tiene permisos administrativos sobre la materia
+    DOCENTE       // Rol general para el resto del equipo
+}
