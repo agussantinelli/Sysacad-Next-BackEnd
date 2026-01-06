cabb7d3397aaa98af123c35fae987973edae00d0
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-06 18:03:49 -0300

    Agrego UsuarioRequest

---
+++ src/main/java/com/sysacad/backend/dto/UsuarioRequest.java
@@ -0,0 +1,31 @@
+package com.sysacad.backend.dto;
+
+import com.sysacad.backend.modelo.enums.Genero;
+import com.sysacad.backend.modelo.enums.RolUsuario;
+import com.sysacad.backend.modelo.enums.TipoDocumento;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+public class UsuarioRequest {
+    private String legajo;
+    private String password; // Solo entra, nunca sale en el Response
+    private TipoDocumento tipoDocumento;
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private String mail;
+    private LocalDate fechaNacimiento;
+    private Genero genero;
+    private String telefono;
+    private String direccion;
+    private String ciudad;
+    private String fotoPerfil;
+    private LocalDate fechaIngreso;
+    private String tituloAcademico;
+    private RolUsuario rol;
+    private String estado;
+}
