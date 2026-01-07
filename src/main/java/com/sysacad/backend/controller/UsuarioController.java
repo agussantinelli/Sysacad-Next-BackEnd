e48981ba7d88f7c973b4097eff5044ce81ef092f
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-07 13:53:18 -0300

    Corrijo Importar Libreria

--- src/main/java/com/sysacad/backend/controller/UsuarioController.java
+++ src/main/java/com/sysacad/backend/controller/UsuarioController.java
@@ -15,6 +15,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
