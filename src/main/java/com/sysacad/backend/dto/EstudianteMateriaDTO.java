65a6c65b5b3654935b03ca1c5b2a1c75c2f6dc3a
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-16 00:48:49 -0300

    NUEVO: EstudianteMateriaDTO

---
+++ src/main/java/com/sysacad/backend/dto/EstudianteMateriaDTO.java
@@ -0,0 +1,19 @@
+package com.sysacad.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EstudianteMateriaDTO {
+    private UUID idMateria;
+    private String nombre;
+    private Short nivel;
+    private String estado; // "PENDIENTE", "CURSANDO", "REGULAR", "APROBADA"
+    private String nota; // "8", "9.50", "-"
+    private Boolean sePuedeInscribir; // flag booleano
+}
