671fdc410f5a365183d95be0f2726a6142bd385f
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-16 01:21:40 -0300

    Renombro a Matriculacion repositorio

---
+++ src/main/java/com/sysacad/backend/repository/MatriculacionRepository.java
@@ -0,0 +1,14 @@
+package com.sysacad.backend.repository;
+
+import com.sysacad.backend.modelo.Matriculacion;
+import com.sysacad.backend.modelo.Matriculacion.MatriculacionId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface MatriculacionRepository extends JpaRepository<Matriculacion, MatriculacionId> {
+    List<Matriculacion> findByIdIdUsuario(UUID idUsuario);
+}
