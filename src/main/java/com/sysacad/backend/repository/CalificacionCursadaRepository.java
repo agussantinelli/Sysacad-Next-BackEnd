903429548b83c3cd8b1424ad8f454d602e3bec15
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-20 01:31:30 -0300

    Armado Repositories

---
+++ src/main/java/com/sysacad/backend/repository/CalificacionCursadaRepository.java
@@ -0,0 +1,11 @@
+package com.sysacad.backend.repository;
+
+import com.sysacad.backend.modelo.CalificacionCursada;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CalificacionCursadaRepository extends JpaRepository<CalificacionCursada, UUID> {
+}
---
+++ src/main/java/com/sysacad/backend/repository/InscripcionCursadoRepository.java
@@ -0,0 +1,18 @@
+package com.sysacad.backend.repository;
+
+import com.sysacad.backend.modelo.InscripcionCursado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface InscripcionCursadoRepository extends JpaRepository<InscripcionCursado, UUID> {
+    List<InscripcionCursado> findByUsuarioId(UUID idUsuario);
+
+    Optional<InscripcionCursado> findByUsuarioIdAndMateriaId(UUID idUsuario, UUID idMateria);
+
+    List<InscripcionCursado> findByComisionId(UUID idComision);
+}
