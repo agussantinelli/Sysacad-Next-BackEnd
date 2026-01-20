fe464c99855c2b19b39f052c4b84ad8ed4354922
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-20 00:52:59 -0300

    NUEVO: InscripcionExamen

---
+++ src/main/java/com/sysacad/backend/modelo/InscripcionExamen.java
@@ -0,0 +1,30 @@
+package com.sysacad.backend.modelo;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "inscripciones_examen")
+public class InscripcionExamen {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private Usuario usuario;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_detalle_mesa", nullable = false)
+    private DetalleMesaExamen detalleMesaExamen;
+
+    @Column(name = "fecha_inscripcion", nullable = false)
+    private LocalDateTime fechaInscripcion;
+
+    @Column(nullable = false, length = 50)
+    private String estado; // PENDIENTE, APROBADO, RECHAZADO, AUSENTE, DESAPROBADO
+}
