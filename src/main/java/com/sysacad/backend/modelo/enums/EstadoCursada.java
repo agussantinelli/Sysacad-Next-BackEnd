a87d8d7b0cb0c0016b2902c00842e65d3722d450
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-20 01:51:11 -0300

    NUEVO: Enum EstadoCursada

---
+++ src/main/java/com/sysacad/backend/modelo/enums/EstadoCursada.java
@@ -0,0 +1,10 @@
+package com.sysacad.backend.modelo.enums;
+
+public enum EstadoCursada {
+    CURSANDO,
+    REGULAR,
+    PROMOCIONADO,
+    LIBRE,
+    APROBADO // Check if this is needed, or if regular + final covers it. Keeping for
+             // consistency.
+}
