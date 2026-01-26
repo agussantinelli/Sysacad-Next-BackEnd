96a1160a38078640a7c69cef99d3bf51421c8be3
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-26 02:10:55 -0300

    NUEVO: MensajeGrupo Modelo

---
+++ src/main/java/com/sysacad/backend/modelo/MensajeGrupo.java
@@ -0,0 +1,30 @@
+package com.sysacad.backend.modelo;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "mensajes_grupo")
+public class MensajeGrupo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_grupo", nullable = false)
+    private Grupo grupo;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private Usuario usuario; // Remitente
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String contenido;
+
+    @Column(name = "fecha_envio")
+    private LocalDateTime fechaEnvio = LocalDateTime.now();
+}
