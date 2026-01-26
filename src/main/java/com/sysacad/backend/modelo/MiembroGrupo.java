848bde9e90dd1975dea6aa82fb24eddeee66bbfd
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-26 02:11:08 -0300

    NUEVO: MiembroGrupo Modelo

---
+++ src/main/java/com/sysacad/backend/modelo/MiembroGrupo.java
@@ -0,0 +1,46 @@
+package com.sysacad.backend.modelo;
+
+import com.sysacad.backend.modelo.enums.RolGrupo;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "miembros_grupo")
+public class MiembroGrupo {
+
+    @EmbeddedId
+    private MiembroGrupoId id;
+
+    @ManyToOne
+    @MapsId("idGrupo")
+    @JoinColumn(name = "id_grupo")
+    private Grupo grupo;
+
+    @ManyToOne
+    @MapsId("idUsuario")
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "rol_interno", nullable = false, length = 20)
+    private RolGrupo rol = RolGrupo.MIEMBRO;
+
+    @Column(name = "fecha_union")
+    private LocalDateTime fechaUnion = LocalDateTime.now();
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Embeddable
+    public static class MiembroGrupoId implements Serializable {
+        private UUID idGrupo;
+        private UUID idUsuario;
+    }
+}
