b9acd60130adc2193572e2734d5e1d5312f71193
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-26 02:33:39 -0300

    Agrego Import Faltante

--- src/main/java/com/sysacad/backend/service/GrupoService.java
+++ src/main/java/com/sysacad/backend/service/GrupoService.java
@@ -19,6 +19,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
