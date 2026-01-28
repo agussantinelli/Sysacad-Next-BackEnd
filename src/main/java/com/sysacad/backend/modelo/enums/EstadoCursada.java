0b05c9d72b671a7c938f8190ec6b522d9b46dab2
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-27 23:03:40 -0300

    Ajusto Logica Enum Estado Cursada

--- src/main/java/com/sysacad/backend/modelo/enums/EstadoCursada.java
+++ src/main/java/com/sysacad/backend/modelo/enums/EstadoCursada.java
@@ -4,7 +4,5 @@ public enum EstadoCursada {
     CURSANDO,
     REGULAR,
     PROMOCIONADO,
-    LIBRE,
-    APROBADO // Check if this is needed, or if regular + final covers it. Keeping for
-             // consistency.
+    LIBRE
 }
