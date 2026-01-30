c1ff403252c75311e27de6c655c1a8ba183e47ee
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-30 17:22:44 -0300

    NUEVO: Correlatividad

---
+++ src/main/java/com/sysacad/backend/modelo/Correlatividad.java
@@ -0,0 +1,39 @@
+package com.sysacad.backend.modelo;
+
+import com.sysacad.backend.modelo.enums.TipoCorrelatividad;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "correlatividades")
+public class Correlatividad implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id_correlatividad")
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_materia", nullable = false)
+    private Materia materia;
+
+    @ManyToOne
+    @JoinColumn(name = "id_correlativa", nullable = false)
+    private Materia correlativa;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo", nullable = false)
+    private TipoCorrelatividad tipo;
+
+    public Correlatividad(Materia materia, Materia correlativa, TipoCorrelatividad tipo) {
+        this.materia = materia;
+        this.correlativa = correlativa;
+        this.tipo = tipo;
+    }
+}
