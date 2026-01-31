27cceac6a60a4a7b21f93fe81481158ee6e13747
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-31 03:13:53 -0300

    NUEVO: ComisionDisponibleDTO

---
+++ src/main/java/com/sysacad/backend/dto/inscripcion_cursado/ComisionDisponibleDTO.java
@@ -0,0 +1,19 @@
+package com.sysacad.backend.dto.inscripcion_cursado;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ComisionDisponibleDTO {
+    private UUID idComision;
+    private String nombreComision;
+    private String turno;
+    private String ubicacion; // "Aula 305 - Piso 3"
+    private List<String> horarios; // "Lunes 18:00 - 22:00", "Jueves 18:00 - 22:00"
+    private List<String> profesores; // "Nicolas Cabello", "Sandra Civiero"
+}
