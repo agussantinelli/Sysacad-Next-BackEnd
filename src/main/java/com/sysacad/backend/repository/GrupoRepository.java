6c41447ee573c3aaa6f0e7b93000055c28528d83
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-02-06 18:01:27 -0300

    Compila OK

--- src/main/java/com/sysacad/backend/repository/GrupoRepository.java
+++ src/main/java/com/sysacad/backend/repository/GrupoRepository.java
@@ -4,6 +4,7 @@ import com.sysacad.backend.modelo.Grupo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
