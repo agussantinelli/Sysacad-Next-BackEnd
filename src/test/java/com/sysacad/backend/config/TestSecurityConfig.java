43cbf351a7552351ee8350fd00182fa3b994c54a
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-03-23 01:46:37 -0300

    Actualizo Test Security Config

--- src/test/java/com/sysacad/backend/config/TestSecurityConfig.java
+++ src/test/java/com/sysacad/backend/config/TestSecurityConfig.java
@@ -1,15 +1,29 @@
 package com.sysacad.backend.config;
 
 import com.sysacad.backend.config.security.JwtAuthenticationFilter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationProvider;
+import java.io.IOException;
 
 @TestConfiguration
 public class TestSecurityConfig {
 
-    @MockBean
-    private JwtAuthenticationFilter jwtAuthenticationFilter;
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter(null, null) {
+            @Override
+            protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) 
+                    throws ServletException, IOException {
+                filterChain.doFilter(request, response);
+            }
+        };
+    }
 
     @MockBean
     private AuthenticationProvider authenticationProvider;
