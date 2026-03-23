21e6e2804d98bc7da1d50e09967800be6b7b12f9
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-03-23 01:56:55 -0300

    Mejora Test Config

--- src/test/java/com/sysacad/backend/config/TestSecurityConfig.java
+++ src/test/java/com/sysacad/backend/config/TestSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.sysacad.backend.config;
 
 import com.sysacad.backend.config.security.JwtAuthenticationFilter;
+import com.sysacad.backend.config.security.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,10 +10,19 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import java.io.IOException;
 
 @TestConfiguration
+@EnableMethodSecurity
 public class TestSecurityConfig {
+
+    @MockBean
+    private JwtService jwtService;
+
+    @MockBean
+    private UserDetailsService userDetailsService;
 
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() {
