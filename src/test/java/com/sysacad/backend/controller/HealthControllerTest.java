923ed7ecf90ab5261d246571f16807f9dc421882
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-03-23 01:13:12 -0300

    ArmoHealth Controller Test

---
+++ src/test/java/com/sysacad/backend/controller/HealthControllerTest.java
@@ -0,0 +1,33 @@
+package com.sysacad.backend.controller;
+
+import com.sysacad.backend.service.IEmailService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HealthController.class)
+@ActiveProfiles("test")
+class HealthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private IEmailService emailService;
+
+    @Test
+    @DisplayName("Debe retornar UP en el health check")
+    void checkHealth_Success() throws Exception {
+        mockMvc.perform(get("/api/health"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("UP"));
+    }
+}
